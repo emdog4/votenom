fd897ec67673b5a675358ac81c063ab1fe386148
Emery Clark <rclark@atd-us.com>
2018-05-16 12:07:46 -0400

    nominate

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'votes';
+const collection = 'nominations';
+
+
+class Nomination {
+    constructor(name) {
+        this.nominee = name;
+        this.votes = 0;
+    }
+}
+
+
+async function insert(nomination) {
+    try {
+        const client = await MongoClient.connect(url);
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        let r = await db.collection(collection).insertOne(nomination);
+        assert.equal(1, r.insertedCount);
+
+        client.close();
+    } catch(err) {
+        return err.stack;
+    }
+    return 'success';
+}
+
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ecommerce Team Player Awards' });
+  res.render('index', { title: 'Ecommerce Team Player Awards', message: '' });
 });
 
 router.get('/results', function(req, res, next) {
@@ -11,7 +44,16 @@ router.get('/results', function(req, res, next) {
 
 router.post('/nominate', function(req, res, next) {
 
-    res.render('index', { title: 'Express' });
+    // TODO: check existing nomination
+    if (req.body.name.length === 0) {
+        console.log('body empty');
+    } else {
+        let nominee = new Nomination(req.body.name);
+
+        insert(nominee).then(value => {
+            res.render('index', {title : '', message: value});
+        });
+    }
 });
 
 router.post('/vote', function(req, res, next) {
